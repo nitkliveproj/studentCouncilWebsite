1202cf70777c4589f586580ca098c8ee3ee1a157
SONAL001 <35200993+SONAL001@users.noreply.github.com>
2018-06-07 10:06:01 +0530

    represent and footer added

--- src/main_website/urls.py
+++ src/main_website/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns=[
 	path('',views.home,name='home'),
+	path('represent/',views.represent,name='represent'),
 ]
--- src/main_website/views.py
+++ src/main_website/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
 	return render(request,'main_website/home.html')
+def represent(request):
+	return render(request,'main_website/represent.html')
